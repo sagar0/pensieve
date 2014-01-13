74cc8699da1e7e54309716e2749865d40c856ff3
Sagar Vemuri <svemuri@twitter.com>
2014-01-13 00:11:31 -0800

    Enhanced FB login/logout experience.

--- android/src/com/bitcrunch/pensieve/android/MainActivity.java
+++ android/src/com/bitcrunch/pensieve/android/MainActivity.java
@@ -2,43 +2,167 @@ package com.bitcrunch.pensieve.android;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.Menu;
+import android.view.MenuItem;
 
 import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.UiLifecycleHelper;
 
 public class MainActivity extends FragmentActivity {
-    private LoginFragment loginFragment;
+    private static final int LOGIN = 0;
+    private static final int SELECTION = 1;
+    private static final int SETTINGS = 2;
+    private static final int FRAGMENT_COUNT = SETTINGS +1;
+
+    private Fragment[] fragments = new Fragment[FRAGMENT_COUNT];
+
+    private boolean isResumed = false;
+    private MenuItem settings;
+
+    private UiLifecycleHelper uiHelper;
+    private Session.StatusCallback callback =
+        new Session.StatusCallback() {
+        @Override
+        public void call(Session session,
+                SessionState state, Exception exception) {
+            onSessionStateChange(session, state, exception);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if (savedInstanceState == null) {
-            // Add the fragment on initial activity setup
-            loginFragment = new LoginFragment();
-            getSupportFragmentManager()
-            .beginTransaction()
-            .add(android.R.id.content, loginFragment)
-            .commit();
-        } else {
-            // Or set the fragment from restored state info
-            loginFragment = (LoginFragment) getSupportFragmentManager()
-            .findFragmentById(android.R.id.content);
+        uiHelper = new UiLifecycleHelper(this, callback);
+        uiHelper.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getSupportFragmentManager();
+        fragments[LOGIN] = fm.findFragmentById(R.id.loginFragment);
+        fragments[SELECTION] = fm.findFragmentById(R.id.selectionFragment);
+        fragments[SETTINGS] = fm.findFragmentById(R.id.userSettingsFragment);
+
+        FragmentTransaction transaction = fm.beginTransaction();
+        for(int i = 0; i < fragments.length; i++) {
+            transaction.hide(fragments[i]);
+        }
+        transaction.commit();
+    }
+
+    private void showFragment(int fragmentIndex, boolean addToBackStack) {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction transaction = fm.beginTransaction();
+        for (int i = 0; i < fragments.length; i++) {
+            if (i == fragmentIndex) {
+                transaction.show(fragments[i]);
+            } else {
+                transaction.hide(fragments[i]);
+            }
+        }
+        if (addToBackStack) {
+            transaction.addToBackStack(null);
+        }
+        transaction.commit();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        uiHelper.onResume();
+        isResumed = true;
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        uiHelper.onPause();
+        isResumed = false;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        uiHelper.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        uiHelper.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        uiHelper.onSaveInstanceState(outState);
+    }
+
+    private void onSessionStateChange(Session session, SessionState state, Exception exception) {
+        // Only make changes if the activity is visible
+        if (isResumed) {
+            FragmentManager manager = getSupportFragmentManager();
+            // Get the number of entries in the back stack
+            int backStackSize = manager.getBackStackEntryCount();
+            // Clear the back stack
+            for (int i = 0; i < backStackSize; i++) {
+                manager.popBackStack();
+            }
+            if (state.isOpened()) {
+                // If the session state is open:
+                // Show the authenticated fragment
+                showFragment(SELECTION, false);
+            } else if (state.isClosed()) {
+                // If the session state is closed:
+                // Show the login fragment
+                showFragment(LOGIN, false);
+            }
         }
     }
 
-      @Override
-      public void onActivityResult(int requestCode, int resultCode, Intent data) {
-          super.onActivityResult(requestCode, resultCode, data);
-          Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
-      }
+    @Override
+    protected void onResumeFragments() {
+        super.onResumeFragments();
+        Session session = Session.getActiveSession();
+
+        if (session != null && session.isOpened()) {
+            // if the session is already open,
+            // try to show the selection fragment
+            showFragment(SELECTION, false);
+        } else {
+            // otherwise present the splash screen
+            // and ask the person to login.
+            showFragment(LOGIN, false);
+        }
+    }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        // only add the menu when the selection fragment is showing
+        if (fragments[SELECTION].isVisible()) {
+            if (menu.size() == 0) {
+                settings = menu.add(R.string.settings);
+            }
+            return true;
+        } else {
+            menu.clear();
+            settings = null;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.equals(settings)) {
+            showFragment(SETTINGS, true);
+            return true;
+        }
+        return false;
     }
 
 }
